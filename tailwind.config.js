5fd05ed34547334808300bfeb25d411b29ae7b1c
Rises21 <bangkitsetio21@gmail.com>
2023-08-17 01:20:58 +0700

    create section home and about

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        textC: "#838792",
+        backgroundC: "#fff",
+        primary: "#8B9EAC",
+        secondary: "#5A87AC",
+        accent: "#85C6F9",
+      },
+    },
+  },
+  plugins: [],
+};
