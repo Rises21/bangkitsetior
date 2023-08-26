b45a0dfe96208fbd750860c8ee1b3f5523d49873
Rises21 <bangkitsetio21@gmail.com>
2023-08-26 11:49:22 +0700

    edit hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+//header @
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbarFixed");
+  } else {
+    header.classList.remove("navbarFixed");
+  }
+};
+
 const hamburger = document.querySelector("#hamburger");
 const navbarMenu = document.querySelector("#navbarMenu");
+
 hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("navMenu");
+  hamburger.classList.toggle("hamburgerToggler");
   navbarMenu.classList.toggle("navMenu");
 });
+
+//header &
