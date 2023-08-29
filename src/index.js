3b8eb64a9e5b114b3f4622d10781922ca69d78df
Rises21 <bangkitsetio21@gmail.com>
2023-08-29 16:50:10 +0700

    edit navbar menu

--- src/index.js
+++ src/index.js
@@ -12,10 +12,24 @@ window.onscroll = function () {
 
 const hamburger = document.querySelector("#hamburger");
 const navbarMenu = document.querySelector("#navbarMenu");
-
+const linkAnchor = document.getElementsByClassName("anchorLink");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburgerToggler");
   navbarMenu.classList.toggle("navMenu");
 });
+//console.log(window.screen.width, "<<<<<");
+if (window.screen.width > 768) {
+  hamburger.classList.remove("hamburgerToggler");
+  navbarMenu.classList.remove("navMenu");
+}
+
+for (const i of linkAnchor) {
+  i.addEventListener("click", function () {
+    if (window.screen.width < 768) {
+      hamburger.classList.toggle("hamburgerToggler");
+      navbarMenu.classList.toggle("navMenu");
+    }
+  });
+}
 
 //header &
