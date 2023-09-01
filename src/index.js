eacab09c155a0086cd927348f293a82569da6e1e
Rises21 <bangkitsetio21@gmail.com>
2023-09-01 20:35:56 +0700

    fixing some bugs and  create portfolio and footer section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 //header @
+const bodyWidth = document.getElementsByTagName("body");
+const hamburger = document.querySelector("#hamburger");
+const navbarMenu = document.querySelector("#navbarMenu");
+const linkAnchor = document.getElementsByClassName("anchorLink");
+
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
@@ -8,24 +13,28 @@ window.onscroll = function () {
   } else {
     header.classList.remove("navbarFixed");
   }
+  for (const i of linkAnchor) {
+    if (window.scrollY > 480) {
+      i.classList.add("navLinkColor");
+    } else {
+      i.classList.remove("navLinkColor");
+    }
+  }
 };
 
-const hamburger = document.querySelector("#hamburger");
-const navbarMenu = document.querySelector("#navbarMenu");
-const linkAnchor = document.getElementsByClassName("anchorLink");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburgerToggler");
   navbarMenu.classList.toggle("navMenu");
 });
-//console.log(window.screen.width, "<<<<<");
-if (window.screen.width > 768) {
+
+if (bodyWidth[0].offsetWidth > 768) {
   hamburger.classList.remove("hamburgerToggler");
   navbarMenu.classList.remove("navMenu");
 }
 
 for (const i of linkAnchor) {
   i.addEventListener("click", function () {
-    if (window.screen.width < 768) {
+    if (bodyWidth[0].offsetWidth < 768) {
       hamburger.classList.toggle("hamburgerToggler");
       navbarMenu.classList.toggle("navMenu");
     }
